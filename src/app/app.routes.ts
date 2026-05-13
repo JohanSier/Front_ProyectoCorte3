de5a414e5eae6401ea07faeec521462b9963c513
adazag <dazasebastian25@gmail.com>
2026-05-12 20:34:22 -0500

    se realiza el front de la historia correctamente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
-import {DocenteList} from './components/docente-list/docente-list';
-import {DocenteForm} from './components/docente-form/docente-form';
+import { Responsables } from './pages/responsables/responsables';
 
 export const routes: Routes = [
-  { path: 'listar', component: DocenteList },
-  { path: 'crear', component: DocenteForm },
-  { path: '', redirectTo: 'listar', pathMatch: 'full' },
-  { path: '**', redirectTo: 'listar' }
+  { path: '', redirectTo: 'responsables', pathMatch: 'full' },
+  { path: 'responsables', component: Responsables },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import {Navbar} from './components/navbar/navbar';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class AppComponent { }
+export class App {
+  title = 'Comedor Comunitario';
+}
